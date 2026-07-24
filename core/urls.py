242d7f3564317909e1b8c262c45094314bc6ab51
abhii <abhiramos707@gmail.com>
2026-07-24 20:42:26 +0530

    feat: implement Spotify OAuth login initiation

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path("spotify/login/", views.spotify_login, name="spotify_login"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,27 @@
 
 
 # Create your views here.
-from django.shortcuts import render
 
+from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import redirect
+from urllib.parse import urlencode
 def home(request):
     return render(request, 'home.html')
+def spotify_login(request):
+    scope = (
+        "user-read-private "
+        "user-read-email "
+        "playlist-modify-public "
+        "playlist-modify-private"
+    )
+
+    params = {
+        "client_id": settings.SPOTIFY_CLIENT_ID,
+        "response_type": "code",
+        "redirect_uri": settings.SPOTIFY_REDIRECT_URI,
+        "scope": scope,
+    }
+
+    auth_url = "https://accounts.spotify.com/authorize?" + urlencode(params)
+    return redirect(auth_url)
